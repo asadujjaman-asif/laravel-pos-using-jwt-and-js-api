91470a43a7abd9cdc98175eed144d1aecc6e78ac
asadujjaman-asif <mdasadurjjaman@gmail.com>
2023-11-29 00:13:44 +0600

    created some component

---
+++ resources/views/frontend/components/home/brand-list-menu.blade.php
@@ -0,0 +1,21 @@
+<a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static" title="Browse Categories">
+        Brands <i class="icon-angle-down"></i>
+    </a>
+
+<div class="dropdown-menu">
+    <nav class="side-nav">
+        <ul class="menu-vertical sf-arrows">
+            <li class="item-lead"><a href="#">ASUS</a></li>
+            <li class="item-lead"><a href="#">LENEVO</a></li>
+            <li><a href="#">HP</a></li>
+            <li><a href="#">LAVA</a></li>
+            <li><a href="#">SAMSUNG</a></li>
+            <li><a href="#">REALME</a></li>
+            <li><a href="#">OPPO</a></li>
+            <li><a href="#">SONY </a></li>
+            <li><a href="#">DELL</a></li>
+            <li><a href="#">BATA</a></li>
+            <li><a href="#">APEX</a></li>
+        </ul><!-- End .menu-vertical -->
+    </nav><!-- End .side-nav -->
+</div><!-- End .dropdown-menu -->
---
+++ resources/views/frontend/components/home/menu.blade.php
@@ -0,0 +1,117 @@
+<nav class="main-nav">
+    <ul class="menu sf-arrows">
+        <li>
+            <a href="product.html" class="sf-with-ul">Men's</a>
+            <div class="megamenu megamenu-sm">
+                <div class="row no-gutters">
+                    <div class="col-md-6">
+                        <div class="menu-col">
+                            <div class="menu-title">Product Details</div><!-- End .menu-title -->
+                            <ul>
+                                <li><a href="product.html">Default</a></li>
+                                <li><a href="product-centered.html">Centered</a></li>
+                                <li><a href="product-extended.html"><span>Extended Info<span class="tip tip-new">New</span></span></a></li>
+                                <li><a href="product-gallery.html">Gallery</a></li>
+                                <li><a href="product-sticky.html">Sticky Info</a></li>
+                                <li><a href="product-sidebar.html">Boxed With Sidebar</a></li>
+                                <li><a href="product-fullwidth.html">Full Width</a></li>
+                                <li><a href="product-masonry.html">Masonry Sticky Info</a></li>
+                            </ul>
+                        </div><!-- End .menu-col -->
+                    </div><!-- End .col-md-6 -->
+
+                    <div class="col-md-6">
+                        <div class="banner banner-overlay">
+                            <a href="category.html">
+                                <img src="{{asset('assets/frontend/')}}/assets/images/menu/banner-2.jpg" alt="Banner">
+
+                                <div class="banner-content banner-content-bottom">
+                                    <div class="banner-title text-white">New Trends<br><span><strong>spring 2019</strong></span></div><!-- End .banner-title -->
+                                </div><!-- End .banner-content -->
+                            </a>
+                        </div><!-- End .banner -->
+                    </div><!-- End .col-md-6 -->
+                </div><!-- End .row -->
+            </div><!-- End .megamenu megamenu-sm -->
+        </li>
+        <li>
+            <a href="product.html" class="sf-with-ul">Women's</a>
+            <div class="megamenu megamenu-sm">
+                <div class="row no-gutters">
+                    <div class="col-md-6">
+                        <div class="menu-col">
+                            <div class="menu-title">Product Details</div><!-- End .menu-title -->
+                            <ul>
+                                <li><a href="product.html">Default</a></li>
+                                <li><a href="product-centered.html">Centered</a></li>
+                                <li><a href="product-extended.html"><span>Extended Info<span class="tip tip-new">New</span></span></a></li>
+                                <li><a href="product-gallery.html">Gallery</a></li>
+                                <li><a href="product-sticky.html">Sticky Info</a></li>
+                                <li><a href="product-sidebar.html">Boxed With Sidebar</a></li>
+                                <li><a href="product-fullwidth.html">Full Width</a></li>
+                                <li><a href="product-masonry.html">Masonry Sticky Info</a></li>
+                            </ul>
+                        </div><!-- End .menu-col -->
+                    </div><!-- End .col-md-6 -->
+
+                    <div class="col-md-6">
+                        <div class="banner banner-overlay">
+                            <a href="category.html">
+                                <img src="{{asset('assets/frontend/')}}/assets/images/menu/banner-2.jpg" alt="Banner">
+
+                                <div class="banner-content banner-content-bottom">
+                                    <div class="banner-title text-white">New Trends<br><span><strong>spring 2019</strong></span></div><!-- End .banner-title -->
+                                </div><!-- End .banner-content -->
+                            </a>
+                        </div><!-- End .banner -->
+                    </div><!-- End .col-md-6 -->
+                </div><!-- End .row -->
+            </div><!-- End .megamenu megamenu-sm -->
+        </li>
+        <li>
+            <a href="product.html" class="sf-with-ul">Kids's</a>
+            <div class="megamenu megamenu-sm">
+                <div class="row no-gutters">
+                    <div class="col-md-6">
+                        <div class="menu-col">
+                            <div class="menu-title">Product Details</div><!-- End .menu-title -->
+                            <ul>
+                                <li><a href="product.html">Default</a></li>
+                                <li><a href="product-centered.html">Centered</a></li>
+                                <li><a href="product-extended.html"><span>Extended Info<span class="tip tip-new">New</span></span></a></li>
+                                <li><a href="product-gallery.html">Gallery</a></li>
+                                <li><a href="product-sticky.html">Sticky Info</a></li>
+                                <li><a href="product-sidebar.html">Boxed With Sidebar</a></li>
+                                <li><a href="product-fullwidth.html">Full Width</a></li>
+                                <li><a href="product-masonry.html">Masonry Sticky Info</a></li>
+                            </ul>
+                        </div><!-- End .menu-col -->
+                    </div><!-- End .col-md-6 -->
+
+                    <div class="col-md-6">
+                        <div class="banner banner-overlay">
+                            <a href="category.html">
+                                <img src="{{asset('assets/frontend/')}}/assets/images/menu/banner-2.jpg" alt="Banner">
+
+                                <div class="banner-content banner-content-bottom">
+                                    <div class="banner-title text-white">New Trends<br><span><strong>spring 2019</strong></span></div><!-- End .banner-title -->
+                                </div><!-- End .banner-content -->
+                            </a>
+                        </div><!-- End .banner -->
+                    </div><!-- End .col-md-6 -->
+                </div><!-- End .row -->
+            </div><!-- End .megamenu megamenu-sm -->
+        </li>
+        <li>
+            <a href="elements-list.html" class="sf-with-ul">N.Ghuri</a>
+
+            <ul>
+                <li><a href="elements-products.html">About Us</a></li>
+                <li><a href="elements-typography.html">Contact Us</a></li>
+                <li><a href="elements-titles.html">Blog</a></li>
+                <li><a href="elements-banners.html">Comming Soon</a></li>
+                <li><a href="elements-product-category.html">Payment Method</a></li>
+            </ul>
+        </li>
+    </ul><!-- End .menu -->
+</nav><!-- End .main-nav -->
