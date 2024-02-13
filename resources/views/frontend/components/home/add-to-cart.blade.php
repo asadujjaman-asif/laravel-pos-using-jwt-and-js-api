2a55669f4ae479e9a10011cf060af24673bb335b
asadujjaman-asif <mdasadurjjaman@gmail.com>
2024-02-13 12:07:31 +0600

    added add to cart system

--- resources/views/frontend/components/home/add-to-cart.blade.php
+++ resources/views/frontend/components/home/add-to-cart.blade.php
@@ -87,11 +87,7 @@
                                     <label for="size">Size:</label>
                                     <div class="select-custom">
                                         <select name="size" id="size" class="form-control">
-                                            <option value="#" selected="selected">Select a size</option>
-                                            <option value="s">Small</option>
-                                            <option value="m">Medium</option>
-                                            <option value="l">Large</option>
-                                            <option value="xl">Extra Large</option>
+                                           
                                         </select>
                                     </div><!-- End .select-custom -->
                                 </div>
@@ -105,20 +101,22 @@
                                 </div><!-- End .details-filter-row -->
 
                                 <div class="product-details-action">
-                                    <div class="details-action-wrapper">
+                                    <!--<div class="details-action-wrapper">
                                         <a href="#" class="btn-product btn-wishlist" title="Wishlist"><span>Add to Wishlist</span></a>
                                         <a href="#" class="btn-product btn-compare" title="Compare"><span>Add to Compare</span></a>
-                                    </div><!-- End .details-action-wrapper -->
-                                    <a href="#" class="btn-product btn-cart"><span>add to cart</span></a>
+                                    </div> End .details-action-wrapper -->
+                                    <input type="hidden" id="setColor">
+                                    <input type="hidden" id="productId">
+                                    <a href="#" class="btn-product btn-cart" onclick="addToCart()"><span>add to cart</span></a>
                                 </div>
 
-                                <div class="product-details-footer">
+                               <!-- <div class="product-details-footer">
                                     <div class="product-cat">
                                         <span>Category:</span>
                                         <a href="#">Women</a>,
                                         <a href="#">Dresses</a>,
                                         <a href="#">Yellow</a>
-                                    </div><!-- End .product-cat -->
+                                    </div>
 
                                     <div class="social-icons social-icons-sm">
                                         <span class="social-label">Share:</span>
@@ -127,7 +125,7 @@
                                         <a href="#" class="social-icon" title="Instagram" target="_blank"><i class="icon-instagram"></i></a>
                                         <a href="#" class="social-icon" title="Pinterest" target="_blank"><i class="icon-pinterest"></i></a>
                                     </div>
-                                </div>
+                                </div>-->
                             </div>
                         </div>
                     </div>
@@ -165,6 +163,7 @@
             document.getElementById("short_image_3").src=results.shortImages['imageThree'];
             document.getElementById("short_image_4").src=results.shortImages['imageFour'];
 
+            document.getElementById("productId").value=results['id'];
             document.getElementById("productName").innerHTML=results['productName'];
             document.getElementById("productPrice").innerHTML="BDT "+results['salePrice'];
             document.getElementById("ratingsInfo").innerHTML=(`<div class="ratings">
@@ -177,20 +176,62 @@
                 let colorList="";
                let result=results['colors'];
                result.forEach((colors, index)=>{
-                colorList+=(`<a class="active" href="#" style="background: #${colors.color.color_code};"><span class="sr-only">${colors.color.name}</span></a>`); 
+                let ac=(index==0)?"active":"";
+                colorList+=(`<a class="${ac}" href="#" onclick="chooseColor(${colors.color.id})" value="${colors.color.id}" style="background: #${colors.color.color_code};"><span class="sr-only">${colors.color.name}</span></a>`); 
                });
                document.getElementById("colorList").innerHTML=colorList;
             }
             let sizes=results.sizes;
-            let sizeList='<option value="#" selected="selected">Select a size</option>';
+            let sizeList='<option value="" selected="selected">Select a size</option>';
             sizes.forEach((size, index)=>{
                 
-                sizeList+=(`<option value="${size.size.slug}">${size.size.name}</option>`);
+                sizeList+=(`<option value="${size.size.id}">${size.size.name}</option>`);
             });
             document.getElementById("size").innerHTML=sizeList;
             
         }catch(error){
             alert(error.message);
         }
-     }
+    }
+    function chooseColor(color){
+        document.getElementById("setColor").value=color;
+    }
+    async function addToCart(e){
+        let productId=document.getElementById("productId").value;
+        let colorId=document.getElementById("setColor").value;
+        let sizeId=document.getElementById("size").value;
+        let qty=document.getElementById("size").value;
+        if(productId==""){
+            return false;
+        }
+        if(colorId==""){
+            alert("Please choose a color");
+            return false;
+        }
+        if(sizeId==""){
+            alert("Please choose a size");
+            return false;
+        }
+        let formData={
+            product_id: productId,
+            color_id: colorId,
+            size_id: sizeId,
+            qty:qty,
+            store_id:1,
+        };
+        let url = "/add-to-cart/";
+        let response=await axios.post(url, formData);
+        console.log(response);
+        if(response.status == 200 && response.data['status']=='success'){
+            let msg=response.data['msg'];
+            document.getElementById("productId").value;
+            document.getElementById("setColor").value;
+            document.getElementById("size").value;
+            $("#add-to-cart").modal("hide");
+            alert(msg);
+        }else{
+            getInput('message').innerText="Failed! something went wrong";
+        }
+
+    }
 </script> 
